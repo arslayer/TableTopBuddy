dce599f9611bf37b3c498d29f7e2eec2643a4dc9
Davin Asiala <davasi@mbpArs.local>
2025-03-21 23:24:10 -0400

    change to kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-<<<<<<< HEAD
-}
-
-android {
-    namespace = "com.example.tabletopbuddy"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.tabletopbuddy"
-=======
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
@@ -20,7 +10,6 @@ android {
 
     defaultConfig {
         applicationId = "org.cist2931.tabletopbuddy"
->>>>>>> dev2
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -42,28 +31,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-<<<<<<< HEAD
-=======
     kotlinOptions {
         jvmTarget = "11"
     }
     buildFeatures {
         compose = true
     }
->>>>>>> dev2
 }
 
 dependencies {
 
-<<<<<<< HEAD
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-=======
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -80,5 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
->>>>>>> dev2
 }
