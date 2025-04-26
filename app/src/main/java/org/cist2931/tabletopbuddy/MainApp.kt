117f07b3bf039e421fa18f15f1e2df9ed9310ef6
Davin A <33433413+arslayer@users.noreply.github.com>
2025-04-26 01:07:12 -0400

    Remove separate health screen.  Fix error on health adjustment

--- app/src/main/java/org/cist2931/tabletopbuddy/MainApp.kt
+++ app/src/main/java/org/cist2931/tabletopbuddy/MainApp.kt
@@ -3,15 +3,18 @@ package org.cist2931.tabletopbuddy
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,12 +25,12 @@ enum class MainNames {
     Start,
     CoinFlip,
     Roll,
-    Health
 }
 
 
 @Composable
 fun MainScreen(navController: NavController) {
+    val vm: HealthViewModel = viewModel()
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -36,6 +39,31 @@ fun MainScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(color = MaterialTheme.colorScheme.primary, text = "Welcome to TableTopBuddy!")
+        OutlinedTextField(
+            value = vm.totalHealth,
+            onValueChange = { value ->
+                vm.updateHealth(value.filter { it.isDigit() })
+                if (vm.totalHealth.isBlank()) vm.updateHealth("0")
+                },
+            label = {
+                Text(stringResource(R.string.current_health))
+            })
+        OutlinedTextField(
+            value = vm.healthAdjust,
+            onValueChange = { value ->
+                vm.updateAdjustment(value.filter { it.isDigit() })
+                if (vm.healthAdjust.isBlank()) vm.updateAdjustment("0")
+                },
+            label = {Text(stringResource(R.string.health_adjustment))}
+        )
+        Row {
+            Button(
+                onClick = { vm.addHealth() }
+            ) { Text(stringResource(R.string.add))}
+            Button(
+                onClick = { vm.subtractHealth() }
+            ) { Text(stringResource(R.string.subtract))}
+        }
         Button(onClick = {navController.navigate(MainNames.CoinFlip.name)}) { Text(text = stringResource(
             R.string.flip_a_coin
         )
@@ -44,10 +72,10 @@ fun MainScreen(navController: NavController) {
             R.string.roll_dice
         )
         )}
-        Button(onClick = {navController.navigate(MainNames.Health.name)}){ Text(text = stringResource(
-            R.string.track_health
-        )
-        )}
+//        Button(onClick = {navController.navigate(MainNames.Health.name)}){ Text(text = stringResource(
+//            R.string.track_health
+//        )
+//        )}
     }
 }
 
@@ -71,9 +99,9 @@ fun MainApp(){
         composable(route = MainNames.Roll.name) {
             RollScreen(navController)
         }
-        composable(route = MainNames.Health.name) {
-            HealthScreen(navController)
-        }
+//        composable(route = MainNames.Health.name) {
+//            HealthScreen(navController)
+//        }
     }
 }
 
