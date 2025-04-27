d03a8863c0b7ef3a948fe87b81da89d390b14555
Davin A <33433413+arslayer@users.noreply.github.com>
2025-04-27 15:06:34 -0400

    Add flip animation

--- app/src/main/java/org/cist2931/tabletopbuddy/CoinFlipScreen.kt
+++ app/src/main/java/org/cist2931/tabletopbuddy/CoinFlipScreen.kt
@@ -1,12 +1,12 @@
 package org.cist2931.tabletopbuddy
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +16,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,11 +38,29 @@ fun CoinFlipScreen(navController: NavController) {
         Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = flipResult
+//        Image(
+//            painter = painterResource(imageResource),
+//            contentDescription = flipResult
+//        )
+        CoinFlipper(
+            modifier = Modifier.size(100.dp),
+            maxJumpHeight = 400.dp,
+            frontSide = { isRunning, flip ->
+                Coin(
+                    symbolShape = TShape,
+                    enabled = !isRunning,
+                    onClick = flip
+                )
+            },
+            backSide = { isRunning, flip ->
+                Coin(
+                    symbolShape = FShape,
+                    enabled = !isRunning,
+                    onClick = flip
+                )
+            }
         )
-        Text(flipResult)
+//        Text(flipResult)
         Spacer(Modifier.height(20.dp))
         Row {
             Button(onClick = {
